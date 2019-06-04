3077c136ebe33d81f3bb102d5910699164f166d7
Jorrit Tinholt <dejorrit@gmail.com>
2019-06-04 19:51:44 +0200

    Added method for easier getting characters

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,23 +4,28 @@ const find = require('lodash').find
 module.exports = {
   Query: {
     characters: () => data.characters,
-    character: (_, args) => find(data.characters, { id: args.id }),
+    character: (_, args) => find(data.characters, args),
     houses: () => data.houses,
     house: (_, args) => find(data.houses, { id: args.id })
   },
 
   Character: {
     houses: character => character.house_ids.map(id => find(data.houses, { id })).filter(val => !!val),
-    parents: character => character.parent_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    children: character => character.child_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    siblings: character => character.sibling_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    killed: character => character.killed_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    killedBy: character => character.killedBy_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    serves: character => character.serves_ids.map(id => find(data.characters, { id })).filter(val => !!val),
-    servedBy: character => character.servedBy_ids.map(id => find(data.characters, { id })).filter(val => !!val)
+    parents: character => findCharactersByAttribute('parent_ids', character),
+    children: character => findCharactersByAttribute('child_ids', character),
+    siblings: character => findCharactersByAttribute('sibling_ids', character),
+    killed: character => findCharactersByAttribute('killed_ids', character),
+    killedBy: character => findCharactersByAttribute('killedBy_ids', character),
+    serves: character => findCharactersByAttribute('serves_ids', character),
+    servedBy: character => findCharactersByAttribute('servedBy_ids', character),
+    marriedOrEngagedWith: character => findCharactersByAttribute('marriedOrEngagedWith_ids', character)
   },
 
   House: {
     characters: house => data.characters.filter(character => character.house_ids.indexOf(house.id) !== -1).filter(val => !!val)
   }
+}
+
+function findCharactersByAttribute (attribute, character) {
+  return character[attribute].map(id => find(data.characters, { id })).filter(val => !!val)
 }
