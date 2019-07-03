b294019047dabfcd96eec94ca59cc2c19820c3b1
Jorrit Tinholt <dejorrit@gmail.com>
2019-07-03 15:18:15 +0200

    Removed nextjs

--- server/index.js
+++ server/index.js
@@ -1,30 +1,21 @@
 const express = require('express')
 const expressGraphql = require('express-graphql')
 const makeExecutableSchema = require('graphql-tools').makeExecutableSchema
-const next = require('next')
-const dev = process.env.NODE_DEV !== 'production'
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
 
 const typeDefs = require('./typeDefs.js')
 const resolvers = require('./resolvers.js')
 
-nextApp.prepare().then(() => {
-  const app = express()
+const app = express()
 
-  // graphql endpoint
-  app.use('/graphql', expressGraphql({
-    schema: makeExecutableSchema({
-      typeDefs,
-      resolvers
-    }),
-    graphiql: true
-  }))
+// graphql endpoint
+app.use('/graphql', expressGraphql({
+  schema: makeExecutableSchema({
+    typeDefs,
+    resolvers
+  }),
+  graphiql: true
+}))
 
-  // render nextjs app
-  app.get('*', handle)
-
-  app.listen(4000, () => {
-    console.log('Running ThronesQL server on http://localhost:4000/graphql ⚔️')
-  })
+app.listen(4000, () => {
+  console.log('Running ThronesQL server on http://localhost:4000/graphql ⚔️')
 })
